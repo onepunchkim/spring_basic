46e51a195c4c1011fe407789f4e241eadb5879ec
onepunchkim <hyukjin4711@gmail.com>
2025-01-14 18:51:00 +0900

    construct injection study

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,12 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository MemberRepository) {
+        this.memberRepository = MemberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
