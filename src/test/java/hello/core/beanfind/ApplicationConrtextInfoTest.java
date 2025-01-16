ab38394bda231902d94c9d987fa7e619c862ef62
성스러운 성진이 <hyukjin4711@gmail.com>
2025-01-16 18:26:33 +0900

    spring bean

--- src/test/java/hello/core/beanfind/ApplicationConrtextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationConrtextInfoTest.java
@@ -1,11 +1,10 @@
-package hello.core.binfind;
+package hello.core.beanfind;
 
 import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 class ApplicationConrtextInfoTest {
 
