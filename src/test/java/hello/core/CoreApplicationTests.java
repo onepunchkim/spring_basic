a4e8a0d7c942ce330c2db9f787ad59ab51dff089
onepunchkim <hyukjin4711@gmail.com>
2025-01-21 17:44:09 +0900

    spring option

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AutoAppConfig.class)
 class CoreApplicationTests {
 
 	@Test
